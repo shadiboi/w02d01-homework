d6890752a0d82f72328d95578d890ab287b2c9d7
shadiboi <saoutabachi@gmail.com>
2019-03-25 16:18:33 -0600

    1. Data types: data structures

--- js/app.js
+++ js/app.js
@@ -1,1 +1,58 @@
 console.log("runing");
+// ### 1. Data types
+// #### Refresher
+
+// So far we have seen:
+
+// * Strings
+// * Numbers
+// * Booleans
+// * Arrays
+// * Objects
+
+// When these datatypes are combined, we get a **data structure**, for example, an array that contains objects that each contain booleans.
+
+// Suppose you are tasked with creating some software, and it is up to **you** to determine which datatypes and what data structure to use.  For each of the following, write which **data types** you would use to represent the data, and then give a small example of the **data structure**:
+
+//   * A light switch that can be either on or off.
+                //Boolean
+//   * A user's email address.
+                //String
+//   * A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+                // Object
+//   * A list of student names from our class.
+                //Array
+//   * A list of student names from our class, each with a location.
+                //Object
+//   * A list of student names from our class, each with a location and each with a list of favorite tv shows.
+                //Array & Object
+
+// &#x1F534; **Commit:** "data types" //DONE
+
+// ### 2. Take it Easy
+
+// 1. Make an array that holds all of the colors of the rainbow.
+const colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'
+]
+// 2. Write code that will access "blue" from the rainbow array.
+const blue = colorArray[4];
+
+// 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+const me = {
+    name: "Shadi",
+    FavFood: "Ali Baba Grill",
+    hobby: "gym",
+    favDataType: Boolean
+};
+
+
+// 4. Write code that will access your hobby from the object that you just created.
+const meHobby = me.hobby;
+
+// ---
+
+// &#x1F534; **Commit:** "data structures"
+
+// ---
+
+
